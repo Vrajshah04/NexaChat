1aa8bd2c3ac77d248d915c4d87b6f4e7adf7d586
vrajshah04 <svraj9090@gmail.com>
2026-03-11 23:29:47 +0530

    pupeteer error

--- backend/whatsapp/client.js
+++ backend/whatsapp/client.js
@@ -268,6 +268,9 @@ async function createClient(userId) {
 
     s.client = new Client({
         authStrategy: new LocalAuth({ clientId: userId }),
+        puppeteer: {
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        },
         webVersionCache: {
             type: 'remote',
             remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
